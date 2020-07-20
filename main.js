4005e83e8ac46edd6d282cfbcc2bdd0365d7987f
Pythack <59161870+Pythack@users.noreply.github.com>
2020-07-20 12:16:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -53,6 +53,7 @@ xmlhttp.send();
 
 infos_get('https://api.nasa.gov/insight_weather/?api_key=UWtB23fwdeREqFp4mpbpd7quIuO8KXzRvaeclwRT&feedtype=json&ver=1.0', function(data) {
     var actual_sol = Object.keys(data)[0];
+    var actual_sol = data[actual_sol];
     console.log(actual_sol);
     var actual_sol_AT = actual_sol['AT'];
     var max_temp = actual_sol_AT['mx'];
